fd12e7d94824a08a6d9751d55af48a653a831895
samiam567 <apun189@gmail.com>
2020-04-30 14:43:58 -0400

    DDR now exits when you close the window

--- src/danceDanceRevolution/DDRRunner.java
+++ src/danceDanceRevolution/DDRRunner.java
@@ -15,16 +15,27 @@ public class DDRRunner {
 	static Song currentSong;
 	
 	public static void main(String[] args) {
-		Song september = new Song(drawer,"./src/danceDanceRevolution/assets/" + JOptionPane.showInputDialog(drawer, "What song?") + ".dat");
+		Song song = new Song(drawer,"./src/danceDanceRevolution/assets/" + JOptionPane.showInputDialog(drawer, "What song?") + ".dat");
 		
-		//noteTest();
-		september.play();
-		System.out.println(september);
+		
+		song.play();
+		System.out.println(song);
 	
+		while (drawer.getFrame().isShowing()) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+		
+		drawer.stop();
+		System.exit(1);
 		
 	}
 	
-	
+	@Deprecated
 	private static void noteTest() {
 		double noteSpeed = 100;
 		UpNote up = new UpNote(drawer,Settings.height/5,noteSpeed,"./src/danceDanceRevolution/assets/arrowTexturePurple.png");
