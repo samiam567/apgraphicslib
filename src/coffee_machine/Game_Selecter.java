6d80e21f9f484e84ebaf167edac0cbdf660a11b9
Alec Pannunzio <apun189@gmail.com>
2024-04-10 23:43:16 -0400

    added some fun jars

--- src/coffee_machine/Game_Selecter.java
+++ src/coffee_machine/Game_Selecter.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 import LegendOfJava.LegendOfJavaRunner;
 import LegendOfJava2.LegendOfJava2;
-import danceDanceRevolution.DDRRunner;
+import essay_writer.Essay_Generator;
 import jetpack_joyride.JetPack_JoyRide;
 import pong.Pong_runner;
 
@@ -12,7 +12,7 @@ public class Game_Selecter {
 	public static void main(String[] args) {
 		System.out.println("Game Selector!!");
 		
-		String[] gameOptions = new String[] {"Jetpack Joyride","3D Pong","Legend of Java 1", "Legend of Java 2", "Dance Dance Revolution","Essay Writer", "Exit"};
+		String[] gameOptions = new String[] {"Jetpack Joyride","3D Pong","Legend of Java 1", "Legend of Java 2","Essay Writer", "Exit"};
 				
 		
 		
@@ -29,10 +29,8 @@ public class Game_Selecter {
 				new LegendOfJavaRunner();
 			}else if (gameOptions[gameSelection].equals("Legend of Java 2")) {
 				new LegendOfJava2();
-			}else if (gameOptions[gameSelection].equals("Dance Dance Revolution")) {
-				new DDRRunner();
 			}else if (gameOptions[gameSelection].equals("Essay Writer")) {
-				
+				new Essay_Generator();
 			}else {
 				exit = true;
 			}
